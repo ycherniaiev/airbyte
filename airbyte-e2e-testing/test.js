9874cfec47dfcb07455f4124da57e5c6b23a5c5e
Iakov Salikov <salikovpro@gmail.com>
2021-11-01 13:22:54 +0000

    fix: rm unused files

--- airbyte-e2e-testing/test.js
+++
@@ -1,6 +0,0 @@
-const { parse } = require("node-html-parser");
-
-const t = parse(`<div>
-<p>Hello,</p> <p>Follow this link to verify your email address.</p> <p><a href='https://dev-cloud.airbyte.io/verify-email?mode=verifyEmail&oobCode=F1bU9MF66Kc4q5nPLOlI_JXkWOoBnzk--sBev2TUMs4AAAF8vJrTZw&apiKey=AIzaSyChszMXB2uagACPHaK1gXeb7gnLaJEDfU0&lang=en'>https://dev-cloud.airbyte.io/verify-email?mode=verifyEmail&oobCode=F1bU9MF66Kc4q5nPLOlI_JXkWOoBnzk--sBev2TUMs4AAAF8vJrTZw&apiKey=AIzaSyChszMXB2uagACPHaK1gXeb7gnLaJEDfU0&lang=en</a></p> <p>If you didn’t ask to verify this address, you can ignore this email.</p> <p>Thanks,</p> <p>Your Airbyte team</p></div>`);
-
-console.log(t.querySelector("a").getAttribute("href"));
